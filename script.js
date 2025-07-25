9d919893f39b6e604d0d08a6bafc522e3f237322
Arun  Kumar <161482514+Arun-Kumar-Mandleshwar@users.noreply.github.com>
2025-07-26 01:08:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
 function stringChop(str, size) {
   // your code here
+		let l = str.length;
+	let array = [];
+	if (l == 0)
+		return array;
+	for (let i = 0; i < l; i += size) {
+		array.push(str.slice(i, i + size));
+
+	}
+	return array; 
 }
 
 // Do not change the code below
